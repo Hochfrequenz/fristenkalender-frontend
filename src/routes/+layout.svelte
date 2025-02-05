44fbfcc4ab001b7c8a419f88071e0fa75ab634c7
olli <info@oliverlahr.de>
2025-02-05 21:21:11 +0100

    Remove obsolete "Loading" screen from landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,6 @@
   import auth from "$src/auth/authService";
   import { isAuthenticated } from "$src/store";
 
-  let isLoading = true;
-
   const exposedEndpoints = [`${base}/`, `${base}/version/`];
 
   onMount(async () => {
@@ -26,11 +24,10 @@
     }
 
     await auth.checkAuth();
-    isLoading = false;
 
     isAuthenticated.subscribe((value: boolean) => {
       const isExposedEndpoint = exposedEndpoints.includes($page.url.pathname);
-      if (!value && !isLoading && !isExposedEndpoint) {
+      if (!value && !isExposedEndpoint) {
         goto(`${base}/`);
       }
     });
@@ -38,24 +35,18 @@
 
   $: {
     const isExposedEndpoint = exposedEndpoints.includes($page.url.pathname);
-    if (!$isAuthenticated && !isLoading && !isExposedEndpoint) {
+    if (!$isAuthenticated && !isExposedEndpoint) {
       goto(`${base}/`);
     }
   }
 </script>
 
-{#if isLoading}
-  <div class="flex items-center justify-center h-screen">
-    <p>Loading...</p>
-  </div>
-{:else}
-  <div class="flex flex-col h-screen overflow-hidden">
-    <main class="flex-grow flex flex-col min-h-0">
-      <slot />
-    </main>
-    <Footer />
-  </div>
-{/if}
+<div class="flex flex-col h-screen overflow-hidden">
+  <main class="flex-grow flex flex-col min-h-0">
+    <slot />
+  </main>
+  <Footer />
+</div>
 
 <style>
   :global(body) {
