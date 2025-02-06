895ec0bcc1a4888ce190717aab539e9461c157d8
olli <info@oliverlahr.de>
2025-02-06 19:02:01 +0100

    Prettier

--- src/lib/components/shared/icon-logo.svelte
+++ src/lib/components/shared/icon-logo.svelte
@@ -2,8 +2,4 @@
   import { base } from "$app/paths";
 </script>
 
-<img
-  class="me-5 w-[160px]"
-  src="{base}/logo.svg"
-  alt="Hochfrequenz-Logo"
-/>
+<img class="me-5 w-[160px]" src="{base}/logo.svg" alt="Hochfrequenz-Logo" />
