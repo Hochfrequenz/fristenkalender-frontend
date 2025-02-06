907470350963eeb1e449051e6924d8b2500df64e
olli <144932831+OLILHR@users.noreply.github.com>
2025-02-06 13:51:45 +0100

    refactor: replace lucide icons with `companystylesheet` icons (#110)
    * Replace `square-arrow` icon with `login`
    
    * Replace `download` icon
    
    * Replace `logout` icon
    
    * Replace `toggle on/off` icons of glossary component

--- src/lib/components/shared/icon-login.svelte
+++ src/lib/components/shared/icon-login.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import { base } from "$app/paths";
 
-  export let width = "18";
-  export let height = "18";
+  export let width = "20";
+  export let height = "20";
   export let stroke = "currentColor";
 </script>
 
 <img
-  src="{base}/companystylesheet/icons/download.svg"
+  src="{base}/companystylesheet/icons/login.svg"
   {width}
   {height}
-  alt="Download icon"
+  alt="Square arrow icon"
   style="stroke: {stroke}"
+  class="size-5"
 />
