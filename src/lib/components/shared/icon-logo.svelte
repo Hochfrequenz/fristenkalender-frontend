c53ee7cc9af33d29161af66ce7d8476d3a0ddfda
konstantin <konstantin.klein@hochfrequenz.de>
2025-02-06 12:42:02 +0100

    fix(ui): use same css class and SVG file for bottom left logo as in ahbesser (#101)
    ---------
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- src/lib/components/shared/icon-logo.svelte
+++ src/lib/components/shared/icon-logo.svelte
@@ -1,15 +1,9 @@
 <script>
   import { base } from "$app/paths";
-
-  export let size = 40;
 </script>
 
 <img
-  class="me-5"
-  style:width="{size}px"
-  style:height="auto"
-  width={size}
-  height={size}
-  src="{base}/logo.svg"
+  class="me-5 w-[160px]"
+  src="{base}/companystylesheet/logo_weiss.svg"
   alt="Hochfrequenz-Logo"
 />
