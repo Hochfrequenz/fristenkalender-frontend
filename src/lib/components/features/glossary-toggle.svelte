fb95213db6f3dbab35e7e0dd1abf00b71a97cd57
olli <info@oliverlahr.de>
2025-02-06 13:27:24 +0100

    Replace `toggle on/off` icons of glossary component

--- src/lib/components/features/glossary-toggle.svelte
+++ src/lib/components/features/glossary-toggle.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { base } from "$app/paths";
+
   let isOpen = false;
 
   function toggleSidebar() {
@@ -96,38 +98,22 @@
       >
         {#if isOpen}
           <!-- toggle off -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-circle-chevron-left"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="m14 16-4-4 4-4" />
-          </svg>
+          <img
+            src="{base}/companystylesheet/icons/glossar_links.svg"
+            width="28"
+            height="28"
+            alt="Close glossary"
+            style="stroke: currentColor"
+          />
         {:else}
           <!-- toggle on -->
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-circle-chevron-right"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="m10 8 4 4-4 4" />
-          </svg>
+          <img
+            src="{base}/companystylesheet/icons/glossar_rechts.svg"
+            width="28"
+            height="28"
+            alt="Open glossary"
+            style="stroke: currentColor"
+          />
         {/if}
       </button>
     </div>
