8beb8d6857deb4efd8a28108e23e62daf376f3ee
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-02-06 16:30:36 +0100

    fix: final final solution footer adaptions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ export default {
     extend: {
       colors: {
         off_white: "#E7E6E5",
-        ahb_primary: "#EBBEC1",
+        ahb_primary: "#ff569c",
         ahb_secondary: "#F4E0E1",
-        bedingungsbaum_primary: "#E5BD5C",
+        bedingungsbaum_primary: "#e5bd5c",
         bedingungsbaum_secondary: "#F4E2B9",
         ebd_primary: "#8BA2D7",
         ebd_secondary: "#C2CEE9",
-        fristenkalender_primary: "#ABDCD3",
+        fristenkalender_primary: "#abdcd3",
         fristenkalender_secondary: "#D4EDE8",
         fristenkalender_tone: "#73B2A5",
         weichesschwarz: "#25241D",
