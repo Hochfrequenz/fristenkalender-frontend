25d469a556b3365925c01353d851263b4daff4d3
konstantin <konstantin.klein@hochfrequenz.de>
2025-02-07 08:56:11 +0100

    fix: final final solution footer adaptions (#114)
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- src/lib/components/shared/footer-tools.svelte
+++ src/lib/components/shared/footer-tools.svelte
@@ -2,14 +2,15 @@
   export let background = "bg-white";
 </script>
 
-<footer class="flex items-center {background} px-3 py-3">
-  <div class="flex-1 flex justify-center">
-    <div class="flex items-center text-sm text-black/70 gap-10 text-center">
+<footer class="flex items-center {background}">
+  <div class="flex-1 flex justify-center p-[0.5em]">
+    <div class="flex items-center text-sm text-black/70 text-center">
       <a
         href="https://ahb-tabellen.hochfrequenz.de"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-row items-center rounded-full bg-ahb_primary text-white text-lg px-5 py-1
+        style="font:14px/22px Roboto, sans-serif; line-height:1.5;"
+        class="flex flex-row items-center rounded-full bg-ahb_primary text-white text-lg px-2 py-1 m-2
             ring-1
             ring-black/5
             hover:ring-black/10
@@ -20,10 +21,26 @@
         AHB-Tabellen
       </a>
       <a
+        href="https://fristenkalender.hochfrequenz.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        style="font:14px/22px Roboto, sans-serif; line-height:1.5;"
+        class="flex flex-row items-center rounded-full bg-fristenkalender_primary text-white text-lg px-2 py-1 m-2
+            ring-1
+            ring-black/5
+            hover:ring-black/10
+            transition-all
+            duration-300
+            ease-in-out"
+      >
+        Fristenkalender
+      </a>
+      <a
         href="https://bedingungsbaum.hochfrequenz.de"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-row items-center rounded-full bg-bedingungsbaum_primary text-white text-lg px-5 py-1
+        style="font:14px/22px Roboto, sans-serif; line-height:1.5;"
+        class="flex flex-row items-center rounded-full bg-bedingungsbaum_primary text-white text-lg px-2 py-1 m-2
             ring-1
             ring-black/5
             hover:ring-black/10
@@ -37,7 +54,8 @@
         href="https://ebd.hochfrequenz.de"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-row items-center rounded-full bg-ebd_primary text-white text-lg px-5 py-1
+        style="font:14px/22px Roboto, sans-serif; line-height:1.5;"
+        class="flex flex-row items-center rounded-full bg-ebd_primary text-white text-lg px-2 py-1 m-2
             ring-1
             ring-black/5
             hover:ring-black/10
@@ -46,20 +64,6 @@
             ease-in-out"
       >
         Entscheidungsbaumdiagramme
-      </a>
-      <a
-        href="https://fristenkalender.hochfrequenz.de"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="flex flex-row items-center rounded-full bg-fristenkalender_primary text-white text-lg px-5 py-1
-            ring-1
-            ring-black/5
-            hover:ring-black/10
-            transition-all
-            duration-300
-            ease-in-out"
-      >
-        Fristenkalender
       </a>
     </div>
   </div>
